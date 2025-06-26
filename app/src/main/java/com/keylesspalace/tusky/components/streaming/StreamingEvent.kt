6054baaad8f1b5935e2ac07be0680dec32b4bea6
kyori19 <kyori@accelf.net>
2025-06-27 07:55:23 +0900

    [streaming] Announcements

--- app/src/main/java/com/keylesspalace/tusky/components/streaming/StreamingEvent.kt
+++ app/src/main/java/com/keylesspalace/tusky/components/streaming/StreamingEvent.kt
@@ -7,4 +7,12 @@ sealed class StreamingEvent(val stream: StreamType) {
     data class Notification(
         val notification: com.keylesspalace.tusky.entity.Notification,
     ) : StreamingEvent(StreamType.User)
+
+    data class Announcement(
+        val announcement: com.keylesspalace.tusky.entity.Announcement,
+    ) : StreamingEvent(StreamType.User)
+
+    data class AnnouncementDeleted(
+        val id: String,
+    ) : StreamingEvent(StreamType.User)
 }
